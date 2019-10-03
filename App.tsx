9c09f36136d4f48471d9d6fc0f07290a3f172b1d
Vitor Freitas <vitorldfreitas@gmail.com>
2019-10-03 19:04:49 -0300

    feature: fetch default application font-family

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState, Component } from 'react'
+import { View } from 'react-native'
 import { ApolloProvider } from 'react-apollo'
+import * as Font from 'expo-font'
 
 import AppContainer from './src/pages/Tabs'
 import client from './graphql'
 
-export default function App() {
-  return (
-    <ApolloProvider client={client}>
-      <AppContainer />
-    </ApolloProvider>
-  )
+export default class App extends Component {
+  public state = {
+    fontIsLoaded: false
+  }
+
+  componentDidMount() {
+    Font.loadAsync({
+      Poppins: require('./assets/fonts/Poppins-Regular.ttf'),
+      'Poppins Bold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+      'Poppins Medium': require('./assets/fonts/Poppins-Medium.ttf')
+    }).then(() => this.setState({ fontIsLoaded: true }))
+  }
+
+  render() {
+    const { fontIsLoaded } = this.state
+
+    if (!fontIsLoaded) {
+      return <View />
+    }
+
+    return (
+      <ApolloProvider client={client}>
+        <AppContainer />
+      </ApolloProvider>
+    )
+  }
 }
