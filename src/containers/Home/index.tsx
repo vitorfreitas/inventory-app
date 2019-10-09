8a0cac5a48ead70c6ffc75aab389f0dee68b212f
Vitor Freitas <vitorldfreitas@gmail.com>
2019-10-08 23:40:58 -0300

    feature: add toolbar with search input and page title with display options on home page

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,21 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Feather } from '@expo/vector-icons'
 
 import Navbar from '../../components/Navbar'
-import { Content } from './styled'
+import { Content, Toolbar, SearchInput, Row, Heading } from './styled'
 import GridContainer from './GridContainer'
+import * as V from '../../styles/variables'
 
 interface Props {
   data?: object
 }
 
-const HomeContainer: React.SFC<Props> = ({ data }) => (
-  <>
-    <Navbar />
+const HomeContainer: React.SFC<Props> = ({ data }) => {
+  const [visualizationMode, setVisualizationMode] = useState('grid')
 
-    <Content>
-      <GridContainer />
-    </Content>
-  </>
-)
+  return (
+    <>
+      <Navbar />
+
+      <Content>
+        <Toolbar>
+          <Feather name="search" size={25} />
+          <SearchInput placeholder="Procurando o que hoje?" />
+        </Toolbar>
+
+        <Row>
+          <Heading>Produtos</Heading>
+
+          <Feather
+            name="grid"
+            color={visualizationMode === 'grid' ? V.Color.primary : '#757575'}
+            size={20}
+            onPress={() => setVisualizationMode('grid')}
+          />
+          <Feather
+            name="list"
+            color={visualizationMode === 'list' ? V.Color.primary : '#757575'}
+            size={25}
+            style={{ marginLeft: 10 }}
+            onPress={() => setVisualizationMode('list')}
+          />
+        </Row>
+
+        <GridContainer />
+      </Content>
+    </>
+  )
+}
 
 export default HomeContainer
--- src/containers/Home/styled.ts
+++ src/containers/Home/styled.ts
@@ -15,3 +15,29 @@ export const ItemContainer = styled(Ripple).attrs({
   background: #fff;
   border-radius: 4px;
 `
+
+export const Row = styled.View`
+  width: 100%;
+  padding: 15px 25px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+export const Toolbar = styled(Row)`
+  background: #fff;
+  border-color: #eee;
+  border-bottom-width: 1px;
+`
+
+export const SearchInput = styled.TextInput`
+  flex: 1;
+  font-size: 16px;
+  text-align: right;
+`
+
+export const Heading = styled.Text`
+  flex: 1;
+  font-size: 18px;
+  font-family: Poppins;
+`
