064f67d2fec4a3d090b6345de2cbc60d786e3a82
Kelvin CS <kelvinh.job@gmail.com>
2019-10-09 19:50:54 -0300

    Moving App to src folder

---
+++ src/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState, Component } from 'react'
+import { View } from 'react-native'
+import { ApolloProvider } from 'react-apollo'
+import * as Font from 'expo-font'
+
+import AppContainer from './pages/Tabs'
+import client from './graphql'
+
+export default class App extends Component {
+  public state = {
+    fontIsLoaded: false
+  }
+
+  componentDidMount() {
+    Font.loadAsync({
+      Poppins: require('./assets/fonts/Poppins-Regular.ttf'),
+      'Poppins Bold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+      'Poppins Medium': require('./assets/fonts/Poppins-Medium.ttf')
+    }).then(() => this.setState({ fontIsLoaded: true }))
+  }
+
+  render() {
+    const { fontIsLoaded } = this.state
+
+    if (!fontIsLoaded) {
+      return <View />
+    }
+
+    return (
+      <ApolloProvider client={client}>
+        <AppContainer />
+      </ApolloProvider>
+    )
+  }
+}
