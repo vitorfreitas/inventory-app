c1e74bd90c50449f7c9e87bfa91bd8af0db21b04
Vitor Freitas <vitorldfreitas@gmail.com>
2019-10-13 19:52:00 -0300

    feature: create default Text components with global font-family

---
+++ packages/app/src/components/Typography/Text/index.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native'
+
+export const NormalText = styled.Text`
+  font-family: Poppins;
+`
+
+export const MediumText = styled.Text`
+  font-family: 'Poppins Medium';
+`
+
+export const BoldText = styled.Text`
+  font-family: 'Poppins Bold';
+`
