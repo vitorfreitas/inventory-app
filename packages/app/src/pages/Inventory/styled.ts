16d0f5d99e2d6d13d19ab17134ddb16d50fc7087
Kelvin CS <kelvinh.job@gmail.com>
2019-10-16 18:36:54 -0300

    Styles created

---
+++ packages/app/src/pages/Inventory/styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+import { Text } from "@styles/styled";
+
+export const ContentTitle = styled(Text)`
+  font-size: 18px;
+  font-family: "Poppins Medium";
+  color: #212121;
+`;
+
+export const Heading = styled.View`
+  padding: 16px;
+`;
+
+export const Content = styled.ScrollView``;
