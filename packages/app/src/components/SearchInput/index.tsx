eb952545b5ccb80bd57bcb1e88607d5181cddb76
Kelvin CS <kelvinh.job@gmail.com>
2019-10-16 08:48:21 -0300

    SearchInput separated in a new component

---
+++ packages/app/src/components/SearchInput/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FullWidthInput } from "../../styled";
+import { Feather } from "@expo/vector-icons";
+
+type Props = {
+  placeholder?: string;
+};
+
+const SearchInput = ({ placeholder }: Props) => (
+  <>
+    <Feather name="search" size={25} />
+    <FullWidthInput placeholder={placeholder} />
+  </>
+);
+
+export default SearchInput;
