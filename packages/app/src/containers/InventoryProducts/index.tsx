184341222d581e3c9ffa5abed20405b097d8ccde
Kelvin CS <kelvinh.job@gmail.com>
2019-10-16 09:41:42 -0300

    Creating inventory

---
+++ packages/app/src/containers/InventoryProducts/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components/native";
+import ProductCard from "./ProductCard";
+
+const InventoryProducts = () => (
+  <>
+    <ProductCard></ProductCard>
+  </>
+);
+
+export default InventoryProducts;
