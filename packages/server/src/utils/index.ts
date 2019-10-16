8e6fc91d2a078191dfd086de7fb3f1a9a3c22e00
Caique Santana <caique.santana10.cs@gmail.com>
2019-10-16 02:24:00 -0300

    update: add a helper to get mongoose validators errors messages

--- packages/server/src/utils/index.ts
+++ packages/server/src/utils/index.ts
@@ -1,5 +1,7 @@
-function log(...data: any): void {
-  console.log(...data)
+import { Error } from 'mongoose'
+
+function getValidationErrorsMessages(err: Error.ValidationError): string[] {
+  return Object.keys(err.errors).map(error => err.errors[error].toString())
 }
 
-export { log }
+export { getValidationErrorsMessages }
