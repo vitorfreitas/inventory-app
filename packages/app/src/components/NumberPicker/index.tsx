490ccfeee8b9e25d2683b1f3ea59c34f2671a697
Kelvin CS <kelvinh.job@gmail.com>
2019-10-16 09:41:28 -0300

    Styled changed

--- packages/app/src/components/NumberPicker/index.tsx
+++ packages/app/src/components/NumberPicker/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import styled from 'styled-components/native'
-import { Feather } from '@expo/vector-icons'
+import React from "react";
+import styled from "styled-components/native";
+import { Feather } from "@expo/vector-icons";
 
 const Container = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-`
+`;
 
 const Button = styled.TouchableOpacity`
   border-radius: 3px;
   border: 2px solid #000;
-`
+`;
 
 export const Quantity = styled.Text`
   margin: 0 10px;
-  font-size: 20px;
+  font-size: 14px;
   margin-top: 3px;
-  font-family: 'Poppins Medium';
-`
+  font-family: "Poppins Medium";
+`;
 
 const NumberPicker: React.SFC = () => (
   <Container>
@@ -32,6 +32,6 @@ const NumberPicker: React.SFC = () => (
       <Feather size={20} name="plus" />
     </Button>
   </Container>
-)
+);
 
-export default NumberPicker
+export default NumberPicker;
