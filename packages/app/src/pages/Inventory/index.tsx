a7c33e364b19fb46c6ee17d7c6816291a760fc5c
Kelvin CS <kelvinh.job@gmail.com>
2019-10-16 09:36:41 -0300

    Stock renamed to Inventory

--- packages/app/src/pages/Inventory/index.tsx
+++ packages/app/src/pages/Inventory/index.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { View } from "react-native";
 import Navbar from "../../components/Navbar";
 import SearchInput from "../../components/SearchInput";
-import StockProducts from "../../containers/StockProducts";
+import InventoryProducts from "../../containers/InventoryProducts";
 import { Toolbar } from "../../styled";
 
 export default class Inventory extends Component {
@@ -14,7 +14,7 @@ export default class Inventory extends Component {
           <SearchInput placeholder={"Search for products"}></SearchInput>
         </Toolbar>
 
-        <StockProducts></StockProducts>
+        <InventoryProducts />
       </View>
     );
   }
