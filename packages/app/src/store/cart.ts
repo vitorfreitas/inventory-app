34b6f18ec9988e463c92937d22dbacd5a90b78f4
Vitor Freitas <vitorldfreitas@gmail.com>
2019-10-19 22:50:51 -0300

    update: change reducer architecture

--- packages/app/src/store/cart.ts
+++ packages/app/src/store/cart.ts
@@ -1,12 +1,24 @@
-const cartReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_ITEM':
-      return [...state, action.payload]
-    case 'CLEAN_CART':
-      return []
-    default:
-      return state
+import Product from 'shared/interfaces/product'
+import { ICartItem } from 'containers/PointOfSale/interfaces'
+
+const cartReducer = (state: ICartItem[] = [], action) => {
+  const addItemToCart = (item: Product) => [...state, item]
+
+  const removeItemFromCart = (item: Product) => {
+    const filteredCart = state.filter((p) => p.product.id !== item.id)
+
+    return filteredCart
   }
+
+  const cleanCart = () => []
+
+  const types = {
+    ADD_ITEM: addItemToCart,
+    REMOVE_FROM_CART: removeItemFromCart,
+    CLEAN_CART: cleanCart,
+  }
+
+  return types[action.type] ? types[action.type](action.payload) : state
 }
 
 export default cartReducer
