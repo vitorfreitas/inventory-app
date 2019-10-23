2eeb2c2485b059dfdd6b2231159e9aa5dbf78436
Vitor Freitas <vitorldfreitas@gmail.com>
2019-10-23 10:31:03 -0300

    style: remove semis and add 'px' unit to border-radius value

--- packages/app/src/components/Button/index.tsx
+++ packages/app/src/components/Button/index.tsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import styled from 'styled-components/native';
-import Ripple from 'react-native-material-ripple';
+import React from 'react'
+import styled from 'styled-components/native'
+import Ripple from 'react-native-material-ripple'
 
-import * as V from 'styles/variables';
+import * as V from 'styles/variables'
 
 const Container = styled(Ripple)`
-  border-radius: 4;
+  border-radius: 4px;
   padding: 6px 18px;
   background: ${V.Color.primary};
 
   align-items: center;
   align-self: flex-start;
   justify-content: center;
-`;
+`
 
 const Text = styled.Text`
   color: #fff;
   font-size: 16px;
   margin-top: 2px;
   font-family: 'Poppins Medium';
-`;
+`
 
 interface Props {
-  text: string,
+  text: string
   onPress: (args?: any) => any
 }
 
@@ -30,6 +30,6 @@ const Button: React.SFC<Props> = ({ text, onPress }) => (
   <Container onPress={onPress}>
     <Text>{text}</Text>
   </Container>
-);
+)
 
-export default Button;
+export default Button
