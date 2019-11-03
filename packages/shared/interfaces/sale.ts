68f85376607edae07e81c9e0788841569bbe1182
Caique Santana <caique.santana10.cs@gmail.com>
2019-11-03 00:05:48 -0300

    refactor(shared): add sale's interface.

---
+++ packages/shared/interfaces/sale.ts
@@ -0,0 +1,46 @@
+import { Schema } from "mongoose";
+
+interface SaleCompositionItem {
+  id: Schema.Types.ObjectId;
+  quantity: number;
+}
+
+interface SaleComboItem {
+  id: Schema.Types.ObjectId;
+  quantity: number;
+  composition: [SaleCompositionItem];
+}
+
+interface SaleItem {
+  id: Schema.Types.ObjectId;
+  quantity: number;
+  combo: [SaleComboItem];
+  composition: [SaleCompositionItem];
+}
+
+// - Produtos
+//     - Id
+//     - Quantidade
+//     - Combo
+//         - Id
+//         - Quantidade
+//         - Composição
+//           - Id
+//     - Composição
+//         - Id
+//         - Quantidade
+// - Data da venda!
+// - Observação da venda
+// - Cliente
+// - Forma de pagamento
+//     - Id
+//     - Receber em
+// - Desconto aplicado
+
+export default interface Sale {
+  products: [SaleItem];
+  customer: Schema.Types.ObjectId;
+  date: Date;
+  discount: number;
+  note: string;
+}
