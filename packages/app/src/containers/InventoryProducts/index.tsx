fe8612b42ed0992f1da6851b90ccd0c52f97efa0
Vitor Freitas <vitorldfreitas@gmail.com>
2019-11-02 21:46:25 -0300

    style: remove semis

--- packages/app/src/containers/InventoryProducts/index.tsx
+++ packages/app/src/containers/InventoryProducts/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { ScrollView, View } from 'react-native';
-import ProductCard from './ProductCard';
+import React from 'react'
+import { ScrollView, View } from 'react-native'
+import ProductCard from './ProductCard'
 
 type Props = {
-  products: any[];
-};
+  products: any[]
+}
 
 const renderProduct = (product, index) => (
   <ProductCard data={product} key={index} />
-);
+)
 
-const InventoryProducts = ({ products }: Props) => (
+const InventoryProducts: React.SFC<Props> = ({ products }) => (
   <View>{products.map(renderProduct)}</View>
-);
+)
 
-export default InventoryProducts;
+export default InventoryProducts
