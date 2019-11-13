b841852fce6f4fb75d5fa39a03cf1e486759be63
Vitor Freitas <vitorldfreitas@gmail.com>
2019-11-12 21:30:11 -0300

    fix: handle errors with Apollo GraphQL

--- packages/app/src/graphql.ts
+++ packages/app/src/graphql.ts
@@ -1,10 +1,31 @@
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
+import { ApolloLink } from 'apollo-link'
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    )
+
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+})
+
+const httpLink = new HttpLink({
+  uri: 'http://10.0.0.109:3001/graphql',
+  headers: {
+    authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkYzhiY2YwMDk5OWM0M2M1NGE2YjA0YyIsImlhdCI6MTU3MzQzNjY1Nn0.B3HhibI-2EGCHtbjsbj1Sq5jYsODcJFos7nl3yoO4kw'
+  }
+})
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({ uri: 'http://192.168.0.131:3001' })
+  link: ApolloLink.from([errorLink, httpLink])
 })
 
 export default client
