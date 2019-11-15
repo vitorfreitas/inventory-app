c90b52ae6143282658031c056c3c550cbde912d4
Vitor Freitas <vitorldfreitas@gmail.com>
2019-11-15 11:34:17 -0300

    feat: save user's products

--- packages/app/src/pages/PointOfSale/Ingredients.tsx
+++ packages/app/src/pages/PointOfSale/Ingredients.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { gql } from 'apollo-boost'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 import LottieView from 'lottie-react-native'
 
 import { t } from 'locations'
@@ -20,15 +20,27 @@ const FETCH_INGREDIENTS = gql`
   }
 `
 
+const CREATE_PRODUCT = gql`
+  mutation CreateProduct($product: ProductInput!) {
+    createProduct(product: $product) {
+      id
+    }
+  }
+`
+
 interface Props {
   navigation: {
     navigate: (page: string) => void
+    popToTop: () => void
   }
 }
 
 const Ingredients: React.SFC<Props> = ({ navigation }) => {
-  const { data, loading } = useQuery<{ baseProducts: IBaseProduct[] }>(
-    FETCH_INGREDIENTS
+  const { data: queryData, loading } = useQuery<{
+    baseProducts: IBaseProduct[]
+  }>(FETCH_INGREDIENTS)
+  const [createProductMutation, { data: mutationData, error }] = useMutation(
+    CREATE_PRODUCT
   )
   const product: AppProductInput = useSelector((state: any) => state.product)
   const dispatch = useDispatch()
@@ -41,11 +53,27 @@ const Ingredients: React.SFC<Props> = ({ navigation }) => {
   }
 
   const createProduct = () => {
-    console.log(product)
+    const composition = product.ingredients.map(ingredient => ({
+      id: ingredient.id,
+      quantity: +ingredient.quantity
+    }))
+
+    const normalizedProduct = {
+      name: product.name,
+      price: product.price,
+      quantity: +product.quantity,
+      composition
+    }
+
+    createProductMutation({ variables: { product: normalizedProduct } })
   }
 
   const navigateToCreateBaseProduct = () => {
     navigation.navigate('CreateBaseProduct')
+  }
+
+  if (mutationData) {
+    navigation.popToTop()
   }
 
   if (loading) {
@@ -72,7 +100,7 @@ const Ingredients: React.SFC<Props> = ({ navigation }) => {
     <IngredientsContainer
       t={t}
       product={product}
-      ingredients={data.baseProducts}
+      ingredients={queryData.baseProducts}
       selectedIngredients={product.ingredients}
       onChangeIngredient={updateProductsIngredients}
       onCreate={createProduct}
