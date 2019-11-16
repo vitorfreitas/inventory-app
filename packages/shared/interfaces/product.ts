5f53164ed3839c9a784cae01d773086760628133
Vitor Freitas <vitorldfreitas@gmail.com>
2019-11-16 11:57:05 -0300

    style: remove semis

--- packages/shared/interfaces/product.ts
+++ packages/shared/interfaces/product.ts
@@ -1,20 +1,21 @@
-import { Schema } from "mongoose";
+import { Schema } from 'mongoose'
 
 interface comboItem {
-  id: Schema.Types.ObjectId;
-  quantity: number;
+  id: Schema.Types.ObjectId
+  quantity: number
 }
 
 interface compositionItem {
-  id: Schema.Types.ObjectId;
-  quantity: number;
+  id: Schema.Types.ObjectId
+  quantity: number
 }
 
 export default interface Product {
-  name: string;
-  photo: string;
-  price: number;
-  quantity: number;
-  combo: [comboItem];
-  composition: [compositionItem];
+  name: string
+  photo: string
+  price: number
+  quantity: number
+  combo: [comboItem]
+  picture: string
+  composition: [compositionItem]
 }
