122763c83e94706f635375a2524fd307a3ae67d6
Vitor Freitas <vitorldfreitas@gmail.com>
2019-11-16 19:04:53 -0300

    refactor: accept icons on Link

--- packages/app/src/components/Link/index.tsx
+++ packages/app/src/components/Link/index.tsx
@@ -6,30 +6,32 @@ import * as V from 'styles/variables'
 import { MediumText } from 'components/Typography/Text'
 
 const Container = styled(Ripple).attrs({
-  rippleOpacity: 0.1,
+  rippleOpacity: 0.1
 })`
-  padding: 6px 18px;
+  padding: 6px 14px 4px;
   border-radius: 4px;
   background: transparent;
 
+  flex-direction: row;
   align-items: center;
-  align-self: flex-start;
   justify-content: center;
 `
 
 const L = styled(MediumText)`
   font-size: 16px;
-  margin-top: 2px;
   color: ${V.Color.primary};
 `
 
 interface Props {
   onPress?: () => void
+  icon?: any
 }
 
-const Link: React.SFC<Props> = ({ children, onPress }) => (
+const Link: React.SFC<Props> = ({ children, onPress, icon }) => (
   <Container onPress={onPress}>
     <L>{children}</L>
+
+    {icon && icon}
   </Container>
 )
 
