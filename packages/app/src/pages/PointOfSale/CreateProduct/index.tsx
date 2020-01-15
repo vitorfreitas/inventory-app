40e5c4df9d43208a024de5017e47ce32a2209d0a
Vitor Freitas <vitorldfreitas@gmail.com>
2020-01-15 13:02:36 -0300

    fix: create products without base products

--- packages/app/src/pages/PointOfSale/CreateProduct/index.tsx
+++ packages/app/src/pages/PointOfSale/CreateProduct/index.tsx
@@ -3,16 +3,52 @@ import React from 'react'
 import CreateProductContainer from 'containers/CreateProduct'
 import { t } from 'locations'
 import { useDispatch, useSelector } from 'react-redux'
+import { gql } from 'apollo-boost'
+import { useMutation } from 'react-apollo'
+
+const CREATE_PRODUCT = gql`
+  mutation CreateProduct($product: ProductInput!) {
+    createProduct(product: $product) {
+      id
+      name
+      price
+    }
+  }
+`
+
+const FETCH_PRODUCTS = gql`
+  query {
+    products {
+      id
+      name
+      price
+    }
+  }
+`
 
 interface Props {
   navigation: {
     navigate: (page: string) => void
+    popToTop: () => void
   }
 }
 
 const CreateProduct: React.SFC<Props> = ({ navigation }) => {
   const product = useSelector((state: any) => state.product)
   const dispatch = useDispatch()
+  const [createProductMutation, { data: mutationData, error }] = useMutation(
+    CREATE_PRODUCT,
+    {
+      update(cache, { data: { createProduct } }) {
+        const { products } = cache.readQuery({ query: FETCH_PRODUCTS })
+
+        cache.writeQuery({
+          query: FETCH_PRODUCTS,
+          data: { products: products.concat([createProduct]) }
+        })
+      }
+    }
+  )
 
   const handleProductChange = product => {
     dispatch({
@@ -26,7 +62,18 @@ const CreateProduct: React.SFC<Props> = ({ navigation }) => {
   }
 
   const createProduct = () => {
-    console.log(product)
+    const normalizedProduct = {
+      name: product.name,
+      price: product.rawPrice / 10,
+      quantity: +product.quantity,
+      minQuantity: +product.minimumAmount
+    }
+
+    createProductMutation({ variables: { product: normalizedProduct } })
+  }
+
+  if (mutationData) {
+    navigation.popToTop()
   }
 
   return (
